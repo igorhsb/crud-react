e7655dad248e1f36c5771483f92593f227bc8aef
igorhsb <igorhsb37@gmail.com>
2023-01-06 16:01:48 -0300

    logic done

--- frontend/src/Components/users/users.jsx
+++ frontend/src/Components/users/users.jsx
@@ -1,5 +1,12 @@
 import React, {Component} from "react"
 import Main from "../template/main"
+import axios from "axios"
+
+const baseURL = "http://localhost:3001/users"
+const initialState = {
+    user : {name: "", email: "" },
+    list: []
+}
 
 const headerProps = {
     icon: 'users',
@@ -8,10 +15,130 @@ const headerProps = {
 }
 
 export default class UserCrud extends Component{
+    state = {...initialState}
+
+    componentWillMount(){
+        axios(baseURL).then(resp => {
+            this.setState({list: resp.data})
+        })
+    }
+
+    load(user) {
+        this.setState({user})
+    }
+
+    remove(user) {
+        axios.delete(`${baseURL}/${user.id}`).then(resp => {
+            const list = this.getUpdateList(user, false)
+            this.setState({list})
+        })
+    }
+
+    renderTable() {
+        return (
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>                
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        )
+    }
+
+    renderRows(){
+        return this.state.list.map(user => {
+            return (
+                <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn btn-warning" onClick={() => this.load(user)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2" onClick={() => this.remove(user)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    clear() {
+        this.setState({user: initialState.user})
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseURL}/${user.id}` : baseURL
+        axios[method](url, user)
+        .then(resp => {
+            const list = this.getUpdateList(resp.data, true)
+            this.setState({user : initialState.user, list})
+        })
+    }
+
+    getUpdateList(user, add){
+        const list = this.state.list.filter(l => l.id !== user.id)
+        if(add) list.unshift(user)
+        return list
+    }
+
+    updateFields(event) {
+        const user = {...this.state.user}
+        user[event.target.name] = event.target.value
+        this.setState({user})
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control" 
+                            name="name" value={this.state.user.name} onChange={e => this.updateFields(e)} placeholder="Digite um nome!"/>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control" 
+                            name="email" value={this.state.user.email} onChange={e => this.updateFields(e)} 
+                            placeholder="Digite um email!"/>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary" onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+                        <button className="btn btn-secondary ml-2" onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <Main {...headerProps}>
-                Cadastro de usuários
+                {this.renderForm()}
+                {this.renderTable()}
             </Main>
         )
     }
